671f8d6201bfc658613f6961f0921d6bffe5506b
Sudharshan Geddam <darshans1013@gmail.com>
2025-07-26 12:30:48 +0530

    String length program without using strlen

--- Strings/len_of_string.cpp
+++ Strings/len_of_string.cpp
@@ -3,8 +3,13 @@
 int main()
 {
 	char ch[100];
-	int count = 0;
+	int length = 0;
 	printf("Enter a string: ");
 	scanf("%s", &ch);
-	printf(ch);
+	while(ch[length] != '\0')
+	{
+		length++;	
+	}	
+	printf("\nLength of the string is:%d", length);
+	return 0;
 }
